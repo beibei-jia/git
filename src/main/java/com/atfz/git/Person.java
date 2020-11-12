408780f588234d14a85b26c22343d02c7af55f38
ecplise_user <2855923051@qq.com>
2020-11-12 22:15:29 +0800

    chengfa update

--- src/main/java/com/atfz/git/Person.java
+++ src/main/java/com/atfz/git/Person.java
@@ -4,5 +4,11 @@ public class Person {
     public static void main(String[] args) {
     	System.out.println("Hello World...");
     	System.out.println("Hello World'''");
+    	for(int i = 1;i <=9;i++) {
+    		for(int j = 1; j <= i; j++) {
+    			System.out.print(j + "*" + i + "=" + j*i + " ");
+    		}
+    		System.out.println();
+    	}
     }
 }
