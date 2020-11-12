0126b1cc853c4d1c3fc084b02226e85a12b47da4
ecplise_user <2855923051@qq.com>
2020-11-12 17:48:51 +0800

    my first commit in Ecplise...

---
+++ src/test/java/com/atfz/git/Pea.java
@@ -0,0 +1,10 @@
+package com.atfz.git;
+
+public class Pea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("I am a pea....");
+	}
+
+}
