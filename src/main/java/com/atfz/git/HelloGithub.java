538e5e64b10d83963c0413883d2c96620767c9e1
ecplise_user <beibei_jia@aliyun.com>
2020-11-18 13:23:39 +0800

    beibei   _update

---
+++ src/main/java/com/atfz/git/HelloGithub.java
@@ -0,0 +1,11 @@
+package com.atfz.git;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("通过github克隆的项目!");	
+        
+	}
+
+}
